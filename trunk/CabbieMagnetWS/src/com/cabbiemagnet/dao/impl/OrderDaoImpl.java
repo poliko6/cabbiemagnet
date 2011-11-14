421302dc661dcc98fb0159f0fb524cf1fa7ccecd
alexander.lazarov@gmail.com <alexander.lazarov@gmail.com@0cdff05b-43dd-0ecb-a2bd-cf8b0a0080f6>
2011-11-14 01:18:52 +0000

    a bug was fixed that only got only the orders that have been replied , from the database,
    /order/customer/{customerId} works too - get all the orders of the customer and of course no matter if the order is replied or not.

--- trunk/CabbieMagnetWS/src/com/cabbiemagnet/dao/impl/OrderDaoImpl.java
+++ trunk/CabbieMagnetWS/src/com/cabbiemagnet/dao/impl/OrderDaoImpl.java
@@ -42,8 +42,53 @@ public class OrderDaoImpl implements IOrderDao {
 
 	@Override
 	public ArrayList<Order> read(long customerId) {
-		// TODO Auto-generated method stub
-		return null;
+
+		String sqlGetOrder = " SELECT cab_order.id, "
+				+ "customer.name as customer_name, "
+				+ "company.name as company_name, "
+				+ "cab_order_state.name as order_state, "
+				+ "cab_order.time_ordered, "
+				+ "cab_order.time_to_deliver as requested_time_to_deliver, "
+				+ "cab_order.from_location, "
+				+ "cab_order.to_location, "
+				+ "cab_order.customer_note, "
+				+ "cab_order_reply.time_to_deliver as order_reply_time_to_deliver, "
+				+ "cab_order_reply.time_of_reply as order_reply_time, ( "
+				+ "SELECT message.message	"
+				+ "FROM cab_order_Reply "
+				+ "JOIN Message ON message.id = cab_order_reply.message_id "
+				+ "WHERE cab_order.id = cab_order_reply.cab_order_id ) AS order_reply_message "
+				+ "FROM cab_order "
+				+ "JOIN Customer ON customer.id = cab_order.customer_id "
+				+ "JOIN Company ON company.id = cab_order.company_id "
+				+ "JOIN cab_order_state ON cab_order_state.id = cab_order.state_id "
+				+ "LEFT JOIN cab_order_reply ON cab_order.id = cab_order_reply.cab_order_id " 
+				+ "WHERE customer.id = ? "
+				+ "Order by cab_order.id";
+		
+		String sqlGetOrderedCars = "SELECT " +
+				"car.type, " +
+				"cab_order_has_cars.quantity, " +
+				"cab_order_has_cars.cab_order_id " +
+				"FROM cab_order_has_cars " +
+				"JOIN car ON cab_order_has_cars.car_id = car.id " +
+				"WHERE cab_order_has_cars.cab_order_id = ?";
+		
+		Object[] orderArgs = new Object[] { customerId };	// put the arguments for the query
+		
+		ArrayList<Order> orders = (ArrayList<Order>) this.jdbcTemplate.query(sqlGetOrder, orderArgs, new OrderRowMapper());	// query the db for all the orders for this customer
+		
+		// go through each order returned from the db
+		for (int i = 0; i < orders.size(); i++)
+		{
+			Object[] carsArgs = new Object[] { orders.get(i).getId() };	// put the id of the first order in the arguments array
+			ArrayList<OrderedCar> orderSpecificCars = (ArrayList<OrderedCar>) this.jdbcTemplate.query(sqlGetOrderedCars, carsArgs, new OrderedCarRowMapper());	// get the list of specific cars for each order
+			
+			orders.get(i).setCars(orderSpecificCars);	// add the list of order specific cars to each order.
+		}
+		
+		// finally return all the orders 
+		return orders;
 	}
 
 	@Override
@@ -68,13 +113,16 @@ public class OrderDaoImpl implements IOrderDao {
 				+ "JOIN Customer ON customer.id = cab_order.customer_id "
 				+ "JOIN Company ON company.id = cab_order.company_id "
 				+ "JOIN cab_order_state ON cab_order_state.id = cab_order.state_id "
-				+ "JOIN cab_order_reply ON cab_order.id = cab_order_reply.cab_order_id "
+				+ "LEFT JOIN cab_order_reply ON cab_order.id = cab_order_reply.cab_order_id "
 				+ "Order by cab_order.id";
 
-		String sqlGetOrderedCars = "SELECT cab_order_has_cars.cab_order_id, "
-				+ "car.type, " + "cab_order_has_cars.quantity "
-				+ "FROM cab_order_has_cars "
-				+ "JOIN car ON cab_order_has_cars.car_id = car.id";
+
+		String sqlGetOrderedCars = "SELECT " +
+				"car.type, " +
+				"cab_order_has_cars.quantity, " +
+				"cab_order_has_cars.cab_order_id " +
+				"FROM cab_order_has_cars " +
+				"JOIN car ON cab_order_has_cars.car_id = car.id ";
 
 		ArrayList<Order> orders = (ArrayList<Order>) this.jdbcTemplate.query(
 				sqlGetOrder, new OrderRowMapper());	// query the db for all the orders
