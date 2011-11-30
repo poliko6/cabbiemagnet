6a9d98eacd56fb32fca3ec21ef1bf6b590ba4d84
alexander.lazarov <alexander.lazarov@gmail.com>
2011-11-30 15:10:54 +0000

    Customer REST Service and Order REST Service now use Responses to deliver information to the client using them. Returns JSON information where needed.

--- CabbieMagnetWS/WebContent/index.jsp
+++ CabbieMagnetWS/WebContent/index.jsp
@@ -5,13 +5,15 @@
 <h1>Welcome to CabbieMagnet!</h1>
 <h2>List of services:</h2>
 <ol>
-<li><a href="companies">companies/</a></li>
-<li><a href="companies/{changelocation}">companies/{location}</a></li>
-<li><a href="customers">customers</a></li>
-<li><a href="customers">customers/{id}</a> - retrieve customer by ID</li>
-<li><a href="customers/{id}/changename{newname}">customers/{id}/changename/{newname}</a> - change  name</li>
+<li><a href="companies">companies</a></li>
+<li><a href="companies/{location}">companies/{location}</a></li>
+<li><a href="customers">customers</a> - get all customers</li>
+<li><a href="customers/new?id=_&name=_">customers/new?id=_&name=_</a> - new customer by ID, name is optional</li>
+<li><a href="customers/{id}">customers/{id}</a> - retrieve customer by ID</li>
+<li><a href="customers/change?id=_&name=_">customers/change?id=_&name=_</a> - change name for a given ID</li>
 <li><a href="orders">orders</a> - list all the orders</li>
 <li><a href="orders/customer/4521576567">rest/orders/customer/4521576567</a> - get orders for customer</li>
+<li><a href="/orders/new?customer_id=4521576567&company_id=2&time_to_deliver=2011-11-14%2001:13:35&from_loc=Ostergade%2054&to_loc=vestergade">/orders/new?customer_id=4521576567&company_id=2&time_to_deliver=2011-11-14%2001:13:35&from_loc=Ostergade%2054&to_loc=vestergade</a> - create new order and INCLUDE JSON with cars</li>
 
 </ol>
 </body>
--- CabbieMagnetWS/src/com/cabbiemagnet/webservices/customers/CustomersResource.java
+++ CabbieMagnetWS/src/com/cabbiemagnet/webservices/customers/CustomersResource.java
@@ -1,12 +1,20 @@
 package com.cabbiemagnet.webservices.customers;
 
+import java.net.URI;
 import java.util.ArrayList;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import com.cabbiemagnet.dao.ICustomerDao;
 import com.cabbiemagnet.model.Customer;
@@ -14,25 +22,33 @@ import com.cabbiemagnet.webservices.Common;
 
 /**
  * 
- * ================================================================================
+ * ============================================================================
+ * ====
+ * 
  * @author anlazarov
  * @date Nov 13, 2011
  * @time 6:47:51 PM
- *								
- * @project CabbieMagnetWS	
- * @package com.cabbiemagnet.webservices	
+ * 
+ * @project CabbieMagnetWS
+ * @package com.cabbiemagnet.webservices
  * @filename CustomersResource.java
  * @description 
- * ================================================================================
+ *              ==================================================================
+ *              ==============
  */
 @Path("/")
 public class CustomersResource {
 	private ICustomerDao custDao;
 
+	@Context
+	private UriInfo uriInfo;
+
 	public CustomersResource() {
 		custDao = (ICustomerDao) Common.getContext().getBean("custDao");
 	}
-
+	
+	
+	// get all the customer
 	@GET
 	@Produces({ MediaType.APPLICATION_JSON })
 	public ArrayList<Customer> getCustomers() {
@@ -41,44 +57,71 @@ public class CustomersResource {
 
 	@GET
 	@Path("{id}")
-	@Produces({ MediaType.APPLICATION_JSON })
-	public Customer getCustomer(@PathParam("id") long id) {
-		
-		Customer registeredCustomer = custDao.read(id); // check the db for this
-														// customer
+	//@Produces({ MediaType.APPLICATION_JSON })
+	public Response getCustomer(@PathParam("id") long id) {
 
-		if (registeredCustomer == null) // if the customer with id specified
-										// does not exist
-		{
+		Response res;
+
+		// get customer from db
+		Customer c = custDao.read(id);
+
+		// check if customer exists
+		if (c == null) {
+			res = Response.noContent().build();
+		} else {
+			res = Response.ok(c.getName()).build();
+		}
+		return res; // return customer's name only text/plain
+	}
+
+	@POST
+	@Path("/new")
+	public Response createCustomer(@QueryParam("id") long id, 
+			@DefaultValue("User") @QueryParam ("name") String name) {
+		
+		Response res;
+
+		if (custDao.read(id) == null) {
 			// create new customer
 			Customer newCustomer = new Customer(); // create the customer
 			newCustomer.setId(id);
-			newCustomer.setName("User");
+			newCustomer.setName(name);
 			custDao.create(newCustomer); // send it for creation
-			return getCustomer(id); // return the created customer with another
-									// check in the db
+
+			URI cUri = uriInfo.getAbsolutePathBuilder().build();
+			
+			// Response Created that give 
+			res = Response.created(cUri).entity("Created user: " + id + " with name: " + name).build();
+		} else {
+			// no changes where made response 
+			res = Response.notModified().build();
 		}
-		return registeredCustomer; // return customer's information
+		return res; // return customer's information
 	}
 
-//	@POST
-//	@Path("{id}")
-//	@Produces({ MediaType.APPLICATION_JSON })
-//	@Consumes(MediaType.TEXT_PLAIN)
-//	public String PostShit(long id) {
-//		return "Hello, " + id;
-//	}
-	
-	@GET
-	@Path("{id}/changename/{newName}")
-	@Produces({ MediaType.APPLICATION_JSON })
-	public Customer changeCustomerName(@PathParam("id") long id, @PathParam("newName") String newName)
-	{
-		Customer registeredCustomer = custDao.read(id); // check the db for this customer
-		registeredCustomer.setName(newName);
-		custDao.update(registeredCustomer);
+	@PUT
+	@Path("/change")
+	public Response changeCustomerName(@QueryParam("id") long id,
+			@DefaultValue ("User") @QueryParam("name") String newName) {
+		Response res;
+
+		if (newName.equals(" ")) { newName = "User"; }
 		
-		return registeredCustomer;
+		Customer registeredCustomer = custDao.read(id); // check the db for this
+														// customer
+		if (registeredCustomer == null) {
+			res = Response.notModified().build();
+		}
+		// customer exists
+		else {
+			registeredCustomer.setName(newName);
+			custDao.update(registeredCustomer);
+
+			// response HTTP OK 200 and return the name in the body of the message
+			res = Response.ok().entity(registeredCustomer.getName()).build();
+		}
+
+		return res;
 	}
-	
+
 }
